bf2d2e29abb865d9d4d552d791fc1e76b8a9271c
ucjd <134995775+ucjd@users.noreply.github.com>
2024-03-11 17:00:16 -0500

    Update Lock.py

--- Lock.py
+++ Lock.py
@@ -3,8 +3,9 @@ import discord
 import requests
 import json
 from discord.ext import commands
+from discord.utils import get
 
-token = "yourtokenhere"
+token = "Your Token Here"
 
 DiscordHacks = commands.Bot(description='DiscordHacks', command_prefix=';', self_bot=True)
 
@@ -18,31 +19,32 @@ async def lockloop():
     while True:
         if not DiscordHacks.lockgc == []:
             for gcid in DiscordHacks.lockgc:
-                response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337', headers=DiscordHacks.headers)
+                response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=DiscordHacks.headers)
                 if response.status_code == 429:
                     response_c = response.text
                     response_content = json.loads(response_c)
                     lockedsec = response_content.get('retry_after')
                 else:
                     headers = {"Authorization": token}
-                    payload = {"content": "__**Successfully Locked GC Master !**__", "nonce": gcid}
+                    payload = {"content": "__**This group is now locked, you cant kick or add anyone.**__", "nonce": gcid}
                     response = requests.post(url=f'https://discord.com/api/v9/channels/{gcid}/messages', headers=headers, json=payload)
                     for i in range(30):
-                        response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337', headers=DiscordHacks.headers)
+                        response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=DiscordHacks.headers)
         await asyncio.sleep(0.8)
 
 @DiscordHacks.event
 async def on_connect():
-    print("Connected Master !")
-    await lockloop()
+  print("Connected , use  ;lock GCID and  ;unlock GCID  !")
+  await lockloop()
 
 @DiscordHacks.command()
 async def lock(ctx, groupid):
     DiscordHacks.lockgc.extend([groupid] * 1)
 
+
 @DiscordHacks.command()
 async def unlock(ctx, groupid=None):
-    await ctx.send('__**Unlocked the Group Master !**__')
+    await ctx.send('__**Unlocked the GC**__')
     if groupid:
         DiscordHacks.lockgc.pop(groupid)
         print(f'Unlocking {groupid} in 120 seconds Master !')
@@ -50,4 +52,4 @@ async def unlock(ctx, groupid=None):
         DiscordHacks.lockgc.clear()
         print(f'Unlocked All Groups')
 
-DiscordHacks.run(token)
+DiscordHacks.run(token, reconnect=True)
